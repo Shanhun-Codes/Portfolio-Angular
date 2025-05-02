10db6f84cbeb196162d835b706fd2383d817af63
Shanhun-Codes <shanhun.codes@gmail.com>
2025-05-02 12:36:14 -0500

    fixed mobile view navbar spacing, updated resume

--- Travis-Coding/src/app/pages/portfolio/portfolio.component.ts
+++ Travis-Coding/src/app/pages/portfolio/portfolio.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { PortfolioHeaderComponent } from "./portfolio-header/portfolio-header.component";
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, PortfolioHeaderComponent],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
 })
